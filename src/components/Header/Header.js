f4f31800f888b611d3cd321be27041de93574b1e
unknown <julianatonu@gmail.com>
2022-10-24 23:33:28 +0600

    added navbar logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './Header.css'
-
+import logo from  '../../images/logo.jpg'
 
 
 const Header = () => {
@@ -15,16 +15,16 @@ const Header = () => {
 
             <Navbar className='navbar  ' expand="lg">
       <Container>
-        <Navbar.Brand className='title text-warning fs-4' href="#home">Learn Online</Navbar.Brand>
+        <Navbar.Brand className='title text-warning fs-4' href="#home"><img className='logo' src={logo} alt=""/>Learn Online</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="">
-            <NavLink className="px-3 ms-5 nav" to="/">Courses</NavLink>
+            <NavLink className="px-3 ms-5 nav text-warning" to="/">Courses</NavLink>
            
-            <NavLink className="px-3  nav" to="/blog">Blog</NavLink>
-            <NavLink className="px-3  nav" to="/login">Login</NavLink>
-            <NavLink className="px-3  nav" to="/register">Register</NavLink>
-            <NavLink className="px-3  nav" >Light</NavLink>
+            <NavLink className="px-3  nav text-warning" to="/blog">Blog</NavLink>
+            <NavLink className="px-3  nav text-warning" to="/login">Login</NavLink>
+            <NavLink className="px-3  nav text-warning"  to="/register">Register</NavLink>
+            <NavLink className="px-3  nav text-warning" >Light</NavLink>
         
            
           </Nav>
